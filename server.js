08c56a2928d0546127a030dd1dc0df31db62df1c
MatthewNewcomb-Yi <matthewnewcombyi@delta.edu>
2021-10-26 19:23:36 -0400

    add login POST route

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.listen(8000,function(){
 console.log("server is running")
 })
 app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
-	res.json({
-		success: true,
-		message: 'got one user',
-		user: req.params.id
-	})
+        console.log(req.params.id)
+        res.json({
+                success: true,
+                message: 'got one user',
+                user: req.params.id
+        })
 })
 
 const mockUserData=[
@@ -21,33 +21,34 @@ const mockUserData=[
 {name:'Jill'}
 ]
 app.get('/users', function(req,res){
- 	res.json({
- 	 	success: true,
- 	 	message: 'successfully got users. Nice!',
- 	 	users: mockUserData
- 	})
+        res.json({
+                success: true,
+                message: 'successfully got users. Nice!',
+                users: mockUserData
+        })
 })
 
 app.post('/login', function(req,res){
-	const username = req.body.username;
-	const password = req.body.password;
+        const username = req.body.username;
+        const password = req.body.password;
 
-	const mockUsername="billyTheKid";
-	const mockPassword="superSecret";
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
 
-	if (username===mockUsername && password===mockPassword){
-		res.json({
-			success: true,
-			message: 'password and username match!',
-			token: 'encrypted token goes here'
-		})
-	} else {
-		res.json({
-			success: false,
-			message: 'password and username do not match'
-		})
-	}
+        if (username===mockUsername && password===mockPassword){
+                res.json({
+                        success: true,
+                        message: 'password and username match!',
+                        token: 'encrypted token goes here'
+                })
+        } else {
+                res.json({
+                        success: false,
+                        message: 'password and username do not match'
+                })
+        }
 })
 
 app.listen(8000,function(){console.log('server is listening')})
+
 
